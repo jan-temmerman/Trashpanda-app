b6614f6b47a1261ff13fa0609beb55c914868480
Jan Temmerman <jantemme@student.arteveldehs.be>
2020-04-12 22:02:47 +0200

    recognizes start and stop and gives result inbetween, TODO: make it resume after stop

--- App.js
+++ App.js
@@ -7,24 +7,56 @@ export default function App() {
   useEffect(() => {
     Voice.onSpeechStart = () => console.log('speech started');
     Voice.onSpeechEnd = () => console.log('speech ended');
-    Voice.onSpeechResults = (e) => console.log(e);
+    Voice.onSpeechResults = (e) => checkResults(e.value[0]);
     
     return
   }, [])
+
+  const checkResults = (spokenText) => {
+    spokenTextLowered = spokenText.toLowerCase()
+    console.log(spokenTextLowered)
+
+    if(spokenTextLowered.includes('start'))
+      console.log('start detected')
+
+    if(spokenTextLowered.includes('stop')) {
+      let unfilteresResult = spokenTextLowered
+      let filteredResult = unfilteresResult.substring( unfilteresResult.lastIndexOf("start") + 6, unfilteresResult.lastIndexOf("stop") - 1)
+      console.log('stop detected')
+      console.log('\n result: ' + filteredResult)
+      handleMircophone('stop')
+      handleMircophone('start')
+    }
+  }
+
+  const handleMircophone = async(action) => {
+
+    if(action === 'start') {
+      try {
+          await Voice.start('en-US');
+      } catch (e) {
+          console.error(e);
+      }
+    } else if(action === 'stop') {
+      try {
+        await Voice.stop();
+      } catch (e) {
+          console.error(e);
+      }
+    }
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to Trashpanda!!!</Text>
       <Button 
-        onPress={async() => {
-          try {
-            await Voice.start('en-US');
-        } catch (e) {
-            //eslint-disable-next-line
-            console.error(e);
-        }
-        }}
-        title="speech"
+        onPress={() => handleMircophone('start')}
+        title="start listening"
+      />
+
+      <Button 
+        onPress={() => handleMircophone('stop')}
+        title="stop listening"
       />
     </View>
   );
